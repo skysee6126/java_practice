b225d6167957fc94202c6fdf9deb7a4b6ac40873
skysee6126 <chooshinhye@gmail.com>
2021-05-27 22:08:34 +0900

    first commit

---
+++ string.java
@@ -0,0 +1,14 @@
+package java;
+
+public class StingExam {
+  public static void main(String[] args) {
+    String strl = "hello";
+    String str2 = "hello";
+
+    String str3 = new String("hello");
+    String str4 = new String("hello");
+
+
+
+  }
+}
