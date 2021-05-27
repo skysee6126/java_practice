ab6a1c4dbe0fce84eb74cd4b13c31ef7302cd977
skysee6126 <chooshinhye@gmail.com>
2021-05-27 22:21:20 +0900

    String

--- string.java
+++ string.java
@@ -2,13 +2,25 @@ package java;
 
 public class StingExam {
   public static void main(String[] args) {
+    //str1 & str2 have same instance(refer)
     String strl = "hello";
     String str2 = "hello";
 
     String str3 = new String("hello");
     String str4 = new String("hello");
 
+    if (str1 == str2)
+      System.out.println("srt1, str2 have same reference");
+    
+    if (str1 == str3)
+      System.out.println("str1, str3 have same reference");
+
+    if (str3 == str4)
+      System.out.println("str3, str4 have same reference");
 
 
+    System.out.println(str1);
+    System.out.println(str1.substring(3));
+    System.out.println(str1);
   }
 }
