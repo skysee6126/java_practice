59d941a6266956a7052cc52d36b52021ec5d41e5
skysee6126 <chooshinhye@gmail.com>
2021-06-01 17:26:41 +0900

    Constructor

--- Car.java
+++ Car.java
@@ -1,4 +1,8 @@
 public class Car {
   String name;
   int number;
+
+  public Car(String n) {
+    name = n;
+  }
 }
--- CarTest.java
+++ CarTest.java
@@ -1,13 +1,13 @@
 public class CarTest {
   public static void main(String[] args) { // 리턴타입, 메소드명(매개변수 등) {구현}
-    Car c1 = new Car();
-    Car c2 = new Car();
+    Car c1 = new Car("Firecar");
+    Car c2 = new Car("Ambulance");
 
-    c1.name = "Firecar";
+    c1.name = "police car";
     c1.number = 1234;
 
-    c1.name = "Ambulance";
-    c1.number = 1111;
+    c2.name = "Ambulance";
+    c2.number = 1111;
 
     System.out.println(c1.name);
     System.out.println(c1.number);
