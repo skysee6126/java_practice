3e75c47b671771777940245b636add7827399546
skysee6126 <chooshinhye@gmail.com>
2021-06-06 21:23:05 +0900

    Access obj

---
+++ Access.java
@@ -0,0 +1,6 @@
+public class Access {
+  public int p =3;
+  protected int p2 = 4; //same package&extends
+  private int i = 1;
+  int k =2; //within same package
+}
---
+++ AccessTest.java
@@ -0,0 +1,9 @@
+public class AccessTest {
+  public static void main(String[] args){
+    Access obj = new Access();
+    System.out.println(obj.p);
+    System.out.println(obj.p2);
+    System.out.println(obj.k);
+    System.out.println(obj.i);
+  }
+}
